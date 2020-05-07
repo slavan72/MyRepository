c748e61c506ee7ace0bb67b2b0dcc43bbd28e75c
slavan72 <slavan72@mail.ru>
2020-05-07 08:23:25 +0300

    Test

---
+++ Hello.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!!!");
+			Console.WriteLine("Main_1");
+
+            Console.ReadKey();
+        }
+    }
+}
